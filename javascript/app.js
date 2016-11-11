b11ecb00cc74e2e92d0e84b2ed34a5f0a486d8d3
Sabrina Bosco <sabrina@wonderweblabs.io>
2016-11-11 14:35:31 +0100

    implement check lunch logic for show up feedback message

--- javascript/app.js
+++ javascript/app.js
@@ -3,12 +3,29 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+  .controller("LunchCheckController", LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
 
-.controller("LunchCheckController", LunchCheckController);
-LunchCheckController.$inject = ['$scope'];
-function ($scope) {
+    function LunchCheckController($scope) {
+        $scope.checkLunch = function (item) {
+            var checkItems = item;
 
+            if (checkItems === undefined || checkItems === '') {
+                $scope.message = 'Please enter data first';
+            } else {
+                var items = splitString(checkItems, ',');
+                if (items <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else if (items > 3) {
+                    $scope.message = 'Too Much!';
+                }
+            }
+        };
 
-}]);
+        function splitString(strToSplit, seperator) {
+            var arrayOfStrings = strToSplit.split(seperator);
+            return arrayOfStrings.length;
+        }
+    }
 
 })();
